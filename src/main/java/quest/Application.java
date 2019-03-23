4ebb4c12af693791deae0dae1d055cbc7e222d07
aliascage <g.triquivel@gmail.com>
2019-03-23 03:08:38 +0300

    Added yandex semantic library

--- src/main/java/quest/Application.java
+++ src/main/java/quest/Application.java
@@ -1,12 +1,22 @@
 package quest;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.stachek66.nlp.mystem.holding.Factory;
+import ru.stachek66.nlp.mystem.holding.MyStem;
+import ru.stachek66.nlp.mystem.holding.MyStemApplicationException;
+import ru.stachek66.nlp.mystem.holding.Request;
+import ru.stachek66.nlp.mystem.model.Info;
+import scala.Option;
+import scala.collection.JavaConversions;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -26,16 +36,68 @@ public class Application {
         return data;
     }
 
-    public static void main(String[] args) throws IOException, URISyntaxException {
+    private final static MyStem mystemAnalyzer = new Factory("-igd --eng-gr --format json --weight")
+            .newMyStem("3.0", Option.empty()).get();
 
-        String text = init();
-        System.out.println(text);
+    public static void main(String[] args) throws IOException, URISyntaxException, MyStemApplicationException {
 
-        createQuestionWithNumber(text);
+
+        final Iterable<Info> result =
+                JavaConversions.asJavaIterable(
+                        mystemAnalyzer
+                                .analyze(Request.apply("Мэнникс просит у Уоррена письмо Линкольна и зачитывает его вслух."))
+                                .info()
+                                .toIterable());
+
+        for (final Info info : result) {
+            System.out.println(info.initial() + " -> " + info.lex() + " | " + info.rawResponse());
+        }
+
+
+//        String text = init();
+//        System.out.println(text);
+//
+//        createQuestionWithNumber(text);
 
     }
 
     private static void createQuestionWithNumber(String text) {
-        //todo:
+        System.out.println();
+        System.out.println();
+        for (String sentense : text.split("\\.")) {
+//            System.out.println(sentense);
+//            if (sentense.contains(" в ")) {
+            for (String words : sentense.split("\\s")) {
+                if (isVerb(words)) {
+                    System.out.print("Кто ");
+                    System.out.print(words);
+//                    System.out.print(" ");
+                    String[] parts = sentense.split(words);
+                    if (parts.length > 1) {
+                        System.out.println(parts[1]);
+                    }
+
+                }
+            }
+//            }
+        }
+    }
+
+    private static List<String> ends = Arrays.asList("ешь", "ет", "ем", "ете", "ут", "ют", "ишь", "ит", " ет", "им", "ите", "ат", "ят");
+
+    private static boolean isVerb(String words) {
+        if (words.length() < 5) {
+            return false;
+
+        }
+        boolean isverb = false;
+        for (String end : ends) {
+            isverb = words.endsWith(end);
+            if (isverb) {
+                return true;
+            }
+
+        }
+        return isverb;
     }
 }
